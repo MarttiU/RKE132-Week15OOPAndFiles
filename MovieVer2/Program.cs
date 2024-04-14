c240b1f1d1d6c32049e1e6efbb0340a9de9b3d2e
MarttiU <martti.umbleja@gmail.com>
2024-04-14 21:20:42 +0300

    add week15 code

---
+++ MovieVer2/Program.cs
@@ -0,0 +1,53 @@
+﻿
+List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFile();
+//ReadDataFromArray(data);
+
+foreach(string line in data)
+{
+    string[] tempArray = line.Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(tempArray[0], tempArray[2]);
+    myMovies.Add(newMovie);
+}
+
+foreach(Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favorite movies {movie.Titel} was released on {movie.Year}");
+}
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"/Users/marttiumbleja/Projects/Data/movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Movie
+{
+    string titel; //fields, allkriipsu pole vaja
+    string year;
+
+    public string Titel
+    {
+        get { return titel; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+
+    public Movie(string _titel, string _year)
+    {
+        titel = _titel;
+        year = _year;
+    }
+
+}
---
+++ Week15OOPAndFiles/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] data = GetDataFromMyFile();
+ReadDataFromArray(data);
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"/Users/marttiumbleja/Projects/Data/movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach(string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
